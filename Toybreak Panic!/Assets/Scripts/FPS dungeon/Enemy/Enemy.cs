555e4cdf7ba3cd01f4a08d38ea5b1d5269aa603d
Oscorra <afkoscorra@gmail.com>
2026-05-26 16:08:36 +0200

    enemy patrol added

---
+++ Toybreak Panic!/Assets/Scripts/FPS dungeon/Enemy/Enemy.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    private StateMachine stateMachine;
+    private NavMeshAgent agent;
+    public NavMeshAgent Agent { get => agent; }
+    // Debug
+    [SerializeField]
+    private string currentState;
+    public EnemyPath path;
+    void Start()
+    {
+        stateMachine = GetComponent<StateMachine>();
+        agent = GetComponent<NavMeshAgent>();
+        stateMachine.Initialise();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
